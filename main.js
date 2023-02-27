d2555456a0ff37378ec82a89f758912e0063a34b
cheesysurajcheese <87599069+cheesysurajcheese@users.noreply.github.com>
2023-02-27 17:47:26 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 song = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
 function preload(){
     song = loadSound("music.mp3");
 }
@@ -6,13 +10,33 @@ function setup(){
     canvas = createCanvas(600,500);
     canvas.center();
     video = createCapture(VIDEO);
+    poseNet = ml5.poseNet(video,modelLoaded);
     video.hide();
+    poseNet.on('pose', gotPoses);
 
 }
+function modelLoaded(){
+    console.log('poseNet is initialised');
+}
+
 function draw(){
     image(video, 0, 0, 600, 500);
 }
 function play(){
     song.play();
     
+}
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX + " leftWristY = " + leftWristY);
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX + " rightWristY = " + rightWristY);
+    }
+
+
+
 }
